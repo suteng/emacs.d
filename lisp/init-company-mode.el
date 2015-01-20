36c95e9f86a95598dd42415541a282607f09a9de
suteng <sutengemail@gmail.com>
2015-01-21 00:10:43 +0800

    change color for terminal

--- lisp/init-company-mode.el
+++ lisp/init-company-mode.el
@@ -37,14 +37,35 @@
 
 ;; (global-set-key [tab] 'tab-indent-or-complete)
 
-(setq company-idle-delay nil)
-(setq company-minimum-prefix-length 1)
-(setq company-show-numbers nil)
-(setq company-tooltip-limit 20)
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
 
-(setq company-dabbrev-downcase nil)
-(setq company-dabbrev-ignore-case nil)
-(setq company-tooltip-align-annotations t)
-(setq company-require-match 'never)
+;; (setq company-idle-delay nil)
+;; (setq company-minimum-prefix-length 1)
+;; (setq company-show-numbers nil)
+;; (setq company-tooltip-limit 20)
+
+;; (setq company-dabbrev-downcase nil)
+;; (setq company-dabbrev-ignore-case nil)
+;; (setq company-tooltip-align-annotations t)
+;; (setq company-require-match 'never)
+
+(custom-set-faces
+  '(company-preview
+       ((t (:foreground "darkgray" :underline t))))
+   '(company-preview-common
+        ((t (:inherit company-preview))))
+    '(company-tooltip
+         ((t (:background "lightgray" :foreground "black"))))
+     '(company-tooltip-selection
+          ((t (:background "steelblue" :foreground "white"))))
+      '(company-tooltip-common
+           ((((type x)) (:inherit company-tooltip :weight bold))
+                (t (:inherit company-tooltip))))
+       '(company-tooltip-common-selection
+            ((((type x)) (:inherit company-tooltip-selection :weight bold))
+                 (t (:inherit company-tooltip-selection)))))
 
 (provide 'init-company-mode)
