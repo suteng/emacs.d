4f84ab5f39e23b6ac7af84c4a925252a3a05415f
suteng <suteng@sutengs.local>
2017-08-31 21:32:42 +0800

    add theme interminal

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -4,7 +4,15 @@
     :ensure t
     :init
     :config
+    (set-terminal-parameter nil 'background-mode 'dark)
+    (if (daemonp)
+	(add-hook 'after-make-frame-functions
+		  (lambda (frame)
+		    (set-terminal-parameter nil 'background-mode 'dark)
+		    (load-theme 'solarized t)))
+      (load-theme 'solarized t))
     )
   )
+
 
 (provide 'init-theme)
