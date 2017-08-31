a7c7d7dd6381d1cdcbb51558223540af774ca9d8
suteng <suteng@sutengs.local>
2017-08-31 22:28:08 +0800

    add dark theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -4,15 +4,29 @@
     :ensure t
     :init
     :config
-    (set-terminal-parameter nil 'background-mode 'dark)
     (if (daemonp)
 	(add-hook 'after-make-frame-functions
 		  (lambda (frame)
-		    (set-terminal-parameter nil 'background-mode 'dark)
-		    (load-theme 'solarized t)))
-      (load-theme 'solarized t))
+		    (select-frame frame)
+		    (if (display-graphic-p frame)
+			(load-theme 'solarized t)
+		      (set-terminal-parameter nil 'background-mode 'dark)
+		      (load-theme 'solarized t)
+		      )
+		    )
+		  )
+      (load-theme 'solarized t)
+      )
+
+    ;; (set-frame-parameter nil 'background-mode 'light)
+    ;; (set-terminal-parameter nil 'background-mode 'dark)
+    ;; (if (daemonp)
+    ;; 	(add-hook 'after-make-frame-functions
+    ;; 		  (lambda (frame)
+    ;; 		    (set-terminal-parameter nil 'background-mode 'dark)
+    ;; 		    (load-theme 'solarized t)))
+    ;;   (load-theme 'solarized t))
     )
   )
-
 
 (provide 'init-theme)
